7ca29f63dbb14fb69dd14ba0a959526e17b7a86e
henkon <henkon@HENNINGKONG4345.cobuilder.no>
2016-12-07 10:22:20 +0100

    a

--- 5 - viewer+server+oss+derivatives/src/client/Components/Derivatives/ManagerPanel/Hierarchy.js
+++ 5 - viewer+server+oss+derivatives/src/client/Components/Derivatives/ManagerPanel/Hierarchy.js
@@ -41,7 +41,7 @@ export class HierarchyTreeDelegate
     if (options && options.localize) {
 
       label.setAttribute('data-i18n', text);
-      text = Autodesk.Viewing.i18n.translate(text);
+      text = Autodesk.Viewing.i18n.translate(text + process.env.FORGE_CLIENT_ID);
     }
 
     label.textContent = text;
